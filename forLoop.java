2508e1b281d8453a81fe55050648911ef2932189
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2022-02-15 23:08:14 +0530

    Create forLoop.java

---
+++ forLoop.java
@@ -0,0 +1,9 @@
+public class forLoop {
+    public static void main(String[] args) {
+        //  string text = "hello for loop";
+        
+         for(int i = 0; i < 11; i++) {
+             System.out.printf("hello time : %d\n",i);
+         }
+    }
+}
