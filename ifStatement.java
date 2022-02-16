05d29253fd1855d4ad0cbb0c9dc47c7915c8304a
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2022-02-16 13:36:40 +0530

    Create ifStatement.java

---
+++ ifStatement.java
@@ -0,0 +1,12 @@
+public class ifStatement {
+     public static void main(String[] args) {
+        int x= 110;
+         
+        if(x<=10){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
