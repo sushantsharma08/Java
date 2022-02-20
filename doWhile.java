8ce4a8021ad1c4880f756c8efbf43dabf7475010
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2022-02-20 21:40:43 +0530

    java udemy more code

---
+++ doWhile.java
@@ -0,0 +1,9 @@
+public class doWhile {
+    public static void main(String[] args) {
+        int i = 0;
+         do {
+             System.out.println("loop runing at " + i);
+             i++;
+         } while (i<10);
+    }
+}
--- scanner.java
+++ scanner.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class scanner{
     public static void main(String[] args){
+
         Scanner input = new Scanner(System.in);
-        System.out.println("enter a line of text");
+        System.out.println("enter YOur full name");
         String line = input.nextLine();
-                                    
         System.out.println("You entered : " + line);
+        
+        Scanner valueInt = new Scanner(System.in);
+        System.out.println("enter age");
+        int value = valueInt.nextInt();
+        System.out.println("you entered " + value);
+
+        Scanner salary = new Scanner(System.in);
+        System.out.println("eneter your annual Income");
+        double earning = salary.nextDouble();
+        System.out.println("your Annual Income is " + earning);
     }
 }
