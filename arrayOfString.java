675040f08cadb7065be158b35a471e06f705fecf
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2022-02-24 12:52:53 +0530

    Create arrayOfString.java

---
+++ arrayOfString.java
@@ -0,0 +1,29 @@
+public class arrayOfString {
+    public static void main(String[] args) {
+
+// 1 meathod to make array
+
+        String[] array= new String[4];
+        array[0]= "hello";
+        array[1]= "how";
+        array[2]= "are";
+        array[3]="you";
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf(array[i] + " ");
+        }
+
+
+            System.out.println("\n");
+// meathod 2 to make array : 
+
+        String[] arrayTwo = {"hello ", "how ", "are  ", "you ", "i ", "am ", "array2 "};
+        // System.out.println(arrayTwo[2]);
+        for (int i = 0; i < arrayTwo.length; i++) {
+            System.out.printf(arrayTwo[i]);
+        }
+
+        String text="twstinfg the string type";
+        System.out.println(text);
+    }
+}
