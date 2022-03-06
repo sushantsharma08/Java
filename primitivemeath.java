e20350ab6d52a41b1c21c5f60a8297c21976bb8a
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2022-03-06 23:16:51 +0530

    new code

---
+++ primitivemeath.java
@@ -0,0 +1,12 @@
+class Robot{
+    void greeting(String text){
+        System.out.println(text);
+    }
+}
+
+public class primitivemeath {
+    public static void main(String[] args) {
+        Robot chitthi = new Robot();
+        chitthi.greeting("good morning ppl");
+    }
+}
