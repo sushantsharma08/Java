89eb5bb03fa76417eec4e413252f3d3e4a84794b
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2022-03-11 23:59:16 +0530

    refactor

---
+++ constructors.java
@@ -0,0 +1,33 @@
+
+class Machine {
+     String name;
+    private int code;
+
+    public Machine() {
+        System.out.println("constructor working!!!");
+    }
+
+    public Machine(String name) {
+        System.out.println("second constructor running");
+        this.name = name;
+    }
+
+    public Machine(String name, int code) {
+        System.out.println("third constructor running");
+        this.name = name;
+        this.code = code;
+    }
+}
+
+public class constructors {
+    public static void main(String[] args) {
+        Machine machine1 = new Machine();
+        Machine machine2 = new Machine("saw");
+        Machine machine3 = new Machine("grinder" , 1);
+
+        // String name3 =machine3.name;
+        // System.out.println(machine3);
+        // System.out.println(name3);
+        
+    }
+}
