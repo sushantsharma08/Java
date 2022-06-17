5c0f8ca2748dfb5db312b29d12956f21c6faf7c5
Sushant Sharma <ssushantlb08@gmail.com>
2022-06-17 23:52:48 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,14 +1,11 @@
-class Fact{
-   public clac(){ int x;
-    for(int i=x; i>0; i-- ){
-        i*=i;
-        System.out.println("i");
-    }
-    }
-}
 
 public class Factorial {
+    public static int calcFactorial(int x){
+        if(x==0)
+            return 1;
+        return x*calcFactorial(x-1);
+          }
     public static void main(String[] args) {
-       Fact.clac; 
+        System.out.println(calcFactorial(5));
     }
 }
